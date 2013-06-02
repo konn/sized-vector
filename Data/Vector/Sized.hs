c181a8643a170eec432c1494abd1453865d8d8ee
Hiromi Ishii <konn.jinro@gmail.com>
2013-06-02 17:12:14 +0900

    separated Naturals to another package.

--- Data/Vector/Sized.hs
+++ Data/Vector/Sized.hs
@@ -4,13 +4,15 @@
 module Data.Vector.Sized ( Vector (..), sLength, length, append, foldr
                          , foldl, singleton, zipWith, zipWithSame, toList, fromList
                          , unsafeFromList, fromList', unsafeFromList'
-                         , all, splitAt, drop, take, map, head, tail) where
+                         , all, splitAt, takeAtMost, splitAtMost
+                         , drop, take, map, head, tail) where
 import Control.Applicative
 import Data.Maybe
-import Data.Type.Natural
-import Prelude             hiding (all, drop, foldl, foldr, head, length, map,
-                            splitAt, tail, take, zipWith)
-import Data.Singletons
+import Data.Singletons       hiding (promote)
+import Data.Type.Monomorphic
+import Data.Type.Natural     hiding (promote)
+import Prelude               hiding (all, drop, foldl, foldr, head, length, map,
+                              splitAt, tail, take, zipWith)
 
 data Vector (a :: *) (n :: Nat)  where
   Nil  :: Vector a Z
@@ -92,6 +94,17 @@ take SZ     _         = Nil
 take (SS n) (x :- xs) = x :- take n xs
 take _ _ = error "imposible!"
 
+takeAtMost :: SNat n -> Vector a m -> Vector a (Min n m)
+takeAtMost = (fst .) . splitAtMost
+
+splitAtMost :: SNat n -> Vector a m -> (Vector a (Min n m), Vector a (m :-: n))
+splitAtMost SZ Nil = (Nil, Nil)
+splitAtMost SZ (x :- xs) = (Nil, x :- xs)
+splitAtMost (SS _) Nil = (Nil, Nil)
+splitAtMost (SS n) (x :- xs) =
+  case splitAtMost n xs of
+    (ys, zs) -> (x :- ys, zs)
+
 map :: (a -> b) -> Vector a n -> Vector b n
 map _ Nil       = Nil
 map f (x :- xs) = f x :- map f xs
@@ -101,3 +114,11 @@ head (x :- _) = x
 
 tail :: Vector a (S n) -> Vector a n
 tail (_ :- xs) = xs
+
+instance Monomorphicable (Vector a) where
+  type MonomorphicRep (Vector a) = [a]
+  demote (Monomorphic vec) = toList vec
+  promote [] = Monomorphic Nil
+  promote (x:xs) =
+    case promote xs of
+      Monomorphic vec -> Monomorphic $ x :- vec
